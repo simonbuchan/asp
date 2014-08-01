b9f3591c2616d352c9efdd11b2efe0a98afeffda
Simon Buchan <simon.buchan@skilitics.com>
2014-08-01 16:27:47 +1200

    Nicer make clean, also clean .lex output.

--- build.mk
+++ build.mk
@@ -12,18 +12,21 @@ CXXFLAGS += -g --std=c++1y -I$(SRCDIR)
 
 all: as3
 
-clean:
-	rm -rf *.o as3
-
 # Don't try to build these from %.lex.cc!
 %.lex :
 vpath %.lex.cc $(CURDIR)
 %.lex.cc : %.lex
 	$(LEX) $(LFLAGS) --prefix $* -o $@ $^
+clean::
+	rm -f *.lex.cc
 
 $(OBJECTS) : %.o : %.cc
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
+clean::
+	rm -f *.o
 
 as3 : $(OBJECTS)
 	$(CXX) $(CXXFLAGS) -o $@ $^
+clean::
+	rm -f asp
 
