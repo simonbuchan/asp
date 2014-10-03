bd80d032f5c6f002bb8bd3ed50f04f7503f2b3ab
Simon Buchan <simon.buchan@skilitics.com>
2014-10-03 18:18:54 +1300

    Autogenerate dependency information.

--- build.mk
+++ build.mk
@@ -1,32 +1,42 @@
 # Find source files in the source directory
-SRCDIR=$(dir $(lastword $(MAKEFILE_LIST)))
+SRCDIR := $(dir $(lastword $(MAKEFILE_LIST)))
 vpath % $(SRCDIR)
+vpath %.lex.cc $(CURDIR)
 
-PROGRAM = as3
-OBJECTS = as3.o as3.lex.o
+all :
 
 CXX = g++
 LEX = flex
 
 CXXFLAGS += -g --std=c++1y -I$(SRCDIR)
 
-all: as3
+PROGRAM = as3
+SOURCES = as3.cc as3.lex.cc
+OBJECTS = $(SOURCES:.cc=.o)
+DEPENDS = $(SOURCES:.cc=.d)
+
+all: as3 test-as3
 
 # Don't try to build these from %.lex.cc!
 %.lex :
-vpath %.lex.cc $(CURDIR)
 %.lex.cc : %.lex
 	$(LEX) $(LFLAGS) --prefix $* -o $@ $^
 clean::
 	rm -f *.lex.cc
-
 $(OBJECTS) : %.o : %.cc
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
 clean::
 	rm -f *.o
 
-as3 : $(OBJECTS)
+$(PROGRAM) : $(OBJECTS)
 	$(CXX) $(CXXFLAGS) -o $@ $^
 clean::
-	rm -f asp
+	rm -f $(PROGRAM)
 
+test-as3 : as3
+	$(SRCDIR)tests/run
+
+$(DEPENDS) : %.d : %.cc
+	$(CXX) $(CXXFLAGS) -MM -o $@ $<
+
+include $(DEPENDS)
